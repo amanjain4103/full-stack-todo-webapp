a204f24c76b8cdda06398e526b7c70960f565243
amanjain4103 <amanjain4103@gmail.com>
2020-05-27 07:50:58 +0530

    Updates

---
+++ precache-manifest.477df23205936a0830b68e105bb7ebb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fc51169c9881b507a6e38059bca7e0e",
+    "url": "/todo-frontend/index.html"
+  },
+  {
+    "revision": "70d2a9223946de08cd38",
+    "url": "/todo-frontend/static/css/main.482aa883.chunk.css"
+  },
+  {
+    "revision": "497aee16bccff21d3170",
+    "url": "/todo-frontend/static/js/2.c63fb2a1.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/todo-frontend/static/js/2.c63fb2a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70d2a9223946de08cd38",
+    "url": "/todo-frontend/static/js/main.88b443e6.chunk.js"
+  },
+  {
+    "revision": "71a4c5ad8ff3cccd9514",
+    "url": "/todo-frontend/static/js/runtime-main.4642733a.js"
+  }
+]);
